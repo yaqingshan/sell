9fd27ddd05deede7b49d41b6582ed9c5ea418696
yaqingshan <shanyaqing@sina.com>
2018-12-27 16:48:05 +0800

    'ratings'

--- src/pages/ratings/Ratings.vue
+++ src/pages/ratings/Ratings.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>
-  这是评论页
+  <div class="ratings">
+    <div class="score-wrapper">
+      <div class="score-left">
+        <div class="score">{{seller.score}}</div>
+        <div class="score-title">综合评分</div>
+        <p>高于周边商家93%</p>
+      </div>
+      <div class="score-right">
+        <div class="right-item">
+          <div class="score-title">服务态度</div>
+          <div class="star">
+            <star></star>
+          </div>
+          <div class="star-num">
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Star from '@/common/star/Star'
+import axios from 'axios'
 export default {
-  name: 'Ratings'
+  name: 'Ratings',
+  components: {
+    Star
+  },
+  data () {
+    return {
+      seller: {},
+      ratings: []
+    }
+  },
+  methods: {
+    getData () {
+      // api/goods.json
+      axios.get('api/seller.json').then(
+        this.getDataSuccess
+      )
+    },
+    getDataSuccess (res) {
+      res = res.data
+      if (res.errno === 0) {
+        this.seller = res.seller
+      }
+    }
+  },
+  mounted () {
+    this.getData()
+  }
 }
 </script>
 
